f4a43f36f797ba982a72613b08e35069944b2d73
hriidzhridoy <you@example.com>
2024-08-23 23:56:32 +0600

    navbar added with wrong shit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import LogoC from "./LogoC";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full  px-6 py-8">
+      <div>
+        <LogoC />
+      </div>
+      <div className="flex">
+        {["Services", "Our work", "about us", "Insights", "contact"].map(
+          (each, index) => {
+            <a key={index} className="text-xl capitalize text-teal-50 ">
+              {each}
+            </a>;
+          }
+        )}
+      </div>
+    </div>
+  );
 }
