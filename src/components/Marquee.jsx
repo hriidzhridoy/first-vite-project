f82c23cad363a1c33aa6f27570218b3fdf2190d8
hriidzhridoy <you@example.com>
2024-08-25 22:50:39 +0600

    marquee enhanced

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,35 +3,22 @@ import { motion } from "framer-motion";
 
 export default function Marquee() {
   return (
-    <div className="bg-[#004D43] py-24 rounded-tl-[20px]  rounded-tr-[20px] overflow-hidden">
-      <div className="flex gap-10 items-center  p-8">
-        <motion.h1
-          className="text-[10vw] uppercase font-bold  whitespace-nowrap"
-          initial={{ x: "-100%" }}
-          animate={{ x: "100%" }}
-          transition={{
-            repeat: Infinity,
-            repeatType: "loop",
-            duration: 10,
-            ease: "linear",
-          }}
-        >
-          We are ochis
-        </motion.h1>
-        <motion.h1
-          className="text-[10vw] uppercase font-bold whitespace-nowrap"
-          initial={{ x: "-100%" }}
-          animate={{ x: "100%" }}
-          transition={{
-            repeat: Infinity,
-            repeatType: "loop",
-            duration: 10,
-            ease: "linear",
-          }}
-        >
-          We are ochis
-        </motion.h1>
-      </div>
+    <div className="bg-[#004D43] py-24 rounded-tl-[20px] font-neue-montreal rounded-tr-[20px] overflow-hidden">
+      <motion.div
+        className="flex gap-10 items-center p-8 whitespace-nowrap"
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "loop",
+          duration: 20, // Adjust duration to control the speed
+          ease: "linear",
+        }}
+      >
+        <h1 className="text-[10vw] uppercase font-bold">
+          We are ochis We are ochis We are ochis We are ochis
+        </h1>
+      </motion.div>
     </div>
   );
 }
