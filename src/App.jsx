a987a64b55891d68d32ad34053d8e2e653094cfb
hriidzhridoy <you@example.com>
2024-08-31 20:56:37 +0600

    LocomotiveScroll added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Marquee from "./components/Marquee";
 import About from "./components/About";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
+import LocomotiveScroll from "locomotive-scroll";
 
 export default function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className="bg-zinc-900 w-full  text-white text-xl">
       <Navbar />
